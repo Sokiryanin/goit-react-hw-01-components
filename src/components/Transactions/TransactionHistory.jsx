9be394a41f708ea9653b10ec27b3fe700048cc8f
Sokiryanin <eugene.sidorov@me.com>
2022-11-28 14:08:07 +0200

    update css

--- src/components/Transactions/TransactionHistory.jsx
+++ src/components/Transactions/TransactionHistory.jsx
@@ -1,22 +1,22 @@
 import css from './TransactionHistory.module.css';
 export const TransactionHistory = ({ items }) => {
   return (
-    <table>
+    <table className={css.table}>
       <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+        <tr className={css.thead_tr}>
+          <th className={css.table_th}>Type</th>
+          <th className={css.table_th}>Amount</th>
+          <th className={css.table_th}>Currency</th>
         </tr>
       </thead>
 
       <tbody>
         {items.map(({ id, type, amount, currency }) => {
           return (
-            <tr key={id}>
-              <td>{type}</td>
-              <td>{amount}</td>
-              <td>{currency}</td>
+            <tr className={css.table_tr} key={id}>
+              <td className={css.table_td}>{type}</td>
+              <td className={css.table_td}>{amount}</td>
+              <td className={css.table_td}>{currency}</td>
             </tr>
           );
         })}
