5d727f354870e7d265b71dec851ae82e5c29a8fc
Sokiryanin <eugene.sidorov@me.com>
2023-08-16 21:38:05 +0200

    add profile

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import user from '../user.json';
+import { Profile } from './Profile/Profile';
+
+const { username, tag, location, avatar, stats } = user;
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+    </div>
+  );
+};
